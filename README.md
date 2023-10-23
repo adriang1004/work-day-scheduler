2fff5529f4b15a8e13e4af5db5c6327a348fca28
adrian garcia <adriang1004@gmail.com>
2023-10-23 17:14:10 -0500

    finished local storage save functions

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# work-day-scheduler
+#   Work Day Scheduler
+
+##  Description
+
+The Work Day Scheduler is a simple yet powerful calendar application that enables users to save events for each hour of a typical workday, from 9 AM to 5 PM. This app runs in the browser and uses dynamically updated HTML and CSS powered by jQuery. Day.js is used for date management, making it easier to handle and format date and time.
+
+##  Features
+    -   Daily Planner:  View and plan your work schedule by the hour
+    -   Pin Events: Click the save button to store it locally
+    -   Dynamic Time Coloring:  Time slots are color-coded to indicate whether they are in the past, present, or future, making it easy to see at a glance where you are in your day.
+
+##  Visual
+
+
+##  --
+
--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,18 @@ $(function () {
     function createTimeBlocks() {
       for (let hour = 9; hour <= 17; hour++) { // loops from 9a to 5p
         var timeBlock = $('<div>').addClass('row time-block').attr('id', 'hour-' + hour);
-        var hourDiv = $('<div>').addClass('col-2 col-md-1 hour text-center py-3').text((hour % 12 || 12) + (hour < 12 ? 'AM' : 'PM')); // convert 24-hour time to 12-hour time with AM/PM
+        var hourDiv = $('<div>').addClass('col-2 col-md-1 hour text-center py-3').text((hour % 12 || 12) + (hour < 12 ? 'AM' : 'PM')); // convert 24-hour time to 12-hour time with AM/PM by fixing the midnight/noon issue returning 0
         var textArea = $('<textarea>').addClass('col-8 col-md-10 description').attr('rows', '3');
         var saveBtn = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save').html('<i class="fas fa-save" aria-hidden="true"></i>');
   
-        
+        timeBlock.append(hourDiv, textArea, saveBtn); // append elements to the time block
+        $('#timeblock-container').append(timeBlock); // append the time block to the container
+  
+        // retrieve event from local storage
+        var event = localStorage.getItem('hour-' + hour);
+        if (event) {
+          textArea.val(event);
+        }
   
         // set time block color based on past, present, or future
         var currentHour = dayjs().hour();
@@ -20,14 +27,22 @@ $(function () {
         }
       }
     }
-
+    // save events into local storage
+    function saveEvent() {
+        var hour = $(this).closest('.time-block').attr('id');
+        var eventText = $(this).siblings('.description').val();
+    
+        localStorage.setItem(hour, eventText);
+      }
+    
       // displays the current day
       $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
     
      
       createTimeBlocks();
     
-
+      // event listener for save buttons
+      $('.saveBtn').on('click', saveEvent);
     });
     
 
